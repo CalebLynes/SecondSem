a901efc2008cd9f00022067839a530b617d117be
U-BSD\477934 <477934@SHS-10DEC3222.beaverton.k12.or.us>
2020-02-14 10:51:11 -0800

    Heap Print DONE

--- secondSem/Heap/main.cpp
+++ secondSem/Heap/main.cpp
@@ -3,19 +3,54 @@
 #include <cstring>
 #include <stdlib.h>
 #include <math.h> //floor
+#include <fstream> //files
 
 using namespace std;
 
-void maxHeapify(int (&A)[100], int i, int size, int &count);
-void printHeap(int (&A)[100], int size);
-void printGivenLevel(int (&A)[100], int root, int level, int size);
+//function prototypes
+void maxHeapify(int (&A)[200], int i, int size, int &count);
+void printHeap(int (&A)[200], int root, int space, int i);
 
-//---Main---
+//------------ MAIN ------------//
 int main() {
-  
+
+  fstream newfile;
+  //size = actual # of elements in given array of ints
   int size = 0;
-  char input[] = {"7 6 5 4 3 2 1"};
-  //amount of elements in array
+  //char input[] = {"343 87 904 132 654 64 66 23 475 999 303 404"};
+  char input[100000];
+  
+  //take in input of numbers or a file of numbers
+  cout << "Take in  a) File  or  b) Normal input? (a or b) ";
+  char answer[2];
+  cin.get(answer, 2);
+  cin.get();
+  if (answer[0] == 'a') {
+    //File
+    cout << "Enter file name (including .txt): ";
+    char filename[50];
+    cin.get(filename, 50);
+    cin.get();
+    newfile.open(filename);
+    if (newfile.is_open()) {
+      int n = 0;
+      while (!newfile.eof()) {
+	myfile.get(input[n]);
+	n++;
+      }
+    }
+  }
+  else if (answer[0] == 'b') {
+    //Normal input
+    
+  }
+  else {
+    cout << "Invalid input" << endl;
+    return 0;
+  }
+
+  
+  //find size
   for (int i = 0; i < strlen(input); i++) {
     if (input[i] == ' ') {
       size++;
@@ -23,8 +58,15 @@ int main() {
   }
   size++;
   cout << "Size: " << size << endl;
+
   //empty heap
-  int heap[100];
+  int heap[200];
+  
+  //initialize heap to all 0s
+  //this is important for printing heap later
+  for (int k = 0; k < 100; k++) {
+    heap[k] = 0;
+  }
 
   //add each element to heap
   int j = 0;
@@ -35,12 +77,15 @@ int main() {
     j++;
     a = strtok(NULL, " ");
   }
+
   //max heapify each element (check if its own left and right is higher
   //than root) keeping track of a count. If count > 0, a change was made,
   //restart iteration. If count == 0, no further changes need to be made.
+  cout << "Constructing max-heap..." << endl;
   int count = 1;
   while (count > 0) {
-    cout << "RESTART" << endl;
+    //--used for testing--
+    //cout << "RESTART" << endl;
     count = 0;
     for (int i = 0; i < size; i++) {
       maxHeapify(heap, i, size, count);
@@ -48,65 +93,85 @@ int main() {
   }
   cout << "DONE" << endl;
   cout << endl;
+
+  //print out heap (will be inorder traversal format)
+  cout << "Heap in inorder format: " << endl;
   for (int i = 0; i < size; i++) {
     cout << heap[i] << endl;
   }
   cout << endl;
-  printHeap(heap, size);
+
+  //print heap in tree form
+  cout << "Heap in tree format: " << endl;
+  printHeap(heap, heap[0], 0, 0);
   
   return 0;
-}
 
-void maxHeapify(int (&A)[100], int i, int size, int &count) {
+}
+//------------ END MAIN ------------//
+
+
+//---Max Heapify
+void maxHeapify(int (&A)[200], int i, int size, int &count) {
     int largest = i;
     int left = 2*i+1;
     int right = 2*i+2;
-    cout << "i: " << A[largest] << ",  ";
-    cout << "left: " << A[left] << ",  ";
-    cout << "right: " << A[right] << endl;
-    
+    //--used for testing--
+    //cout << "i: " << A[largest] << ",  ";
+    //cout << "left: " << A[left] << ",  ";
+    //cout << "right: " << A[right] << endl;
+
+    //if current roots left is greater than itself
     if (A[left] > A[largest] && left <= size) {
-        largest = left;
+      //swap indexes
+      largest = left;
     }
+    
+    //if current root's right is greater than itself
     if (A[right] > A[largest] && right <= size) {
-        largest = right;
+      //swap indexes
+      largest = right;
     }
     if (largest != i) {
-        cout << "Swap " << A[i] << " and " << A[largest] << endl;
-        //a change was made
-        //swap largest and i
-        int temp = A[i];
-        A[i] = A[largest];
-        A[largest] = temp;
-        count++;
-        maxHeapify(A, largest, size, count);
+      //--used for testing--
+      //cout << "Swap " << A[i] << " and " << A[largest] << endl;
+      
+      //IF we get here, a change was made in the indexes!
+      //Swap largest and i
+      int temp = A[i];
+      A[i] = A[largest];
+      A[largest] = temp;
+      
+      //adding to count reports that a change was made
+      //and the heap may still need sorting
+      count++;
+      
+      //call recursively using node index we just changed
+      maxHeapify(A, largest, size, count);
     }
 }
 
-void printHeap(int (&A)[100], int size) {
-  cout << "Size: " << size << endl;
-  int levels = floor(1 + log2(size));
-  cout << "levels: " << levels << endl;
+//---Print Heap
+void printHeap(int (&A)[200], int root, int space, int index) {
   
-  int nodesBeforeLast = pow(2, levels-1) - 1;
-  cout << "Nodes before last: " << nodesBeforeLast << endl;
-  int lastLevelNodes = size - nodesBeforeLast;
-  cout << "Nodes on last level: " << lastLevelNodes << endl;
-
-  for (int i = 1; i <= levels; i++) {
-    printGivenLevel(A, A[0], i, size);
-  }
-}
-
-void printGivenLevel(int (&A)[100], int root, int level, int size) {
+  //base case
   if (root == 0) {
     return;
   }
-  if (level == 1) {
-    cout << root << endl;
+  
+  //increase indentation
+  space += 10;
+
+  //right child
+  printHeap(A, A[2*index+2], space, 2*index+2);
+
+  //print current node after indent
+  cout << endl;
+  for (int j = 10; j < space; j++) {
+    cout << " ";
   }
-  else if (level > 1) {
-    //level is greater than 1
-    
-  }
+  cout << root << "\n";
+
+  //left child
+  printHeap(A, A[2*index+1], space, 2*index+1);
 }
